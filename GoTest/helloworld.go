f2778f777d7f903a2bffb7dfe7c00e04b6c2b167
Remon-Ansary <rasel15-10514@diu.edu.bd>
2022-01-19 11:32:00 +0600

    go

--- GoTest/helloworld.go
+++ GoTest/helloworld.go
@@ -1,65 +1,61 @@
 package main
 
 import (
-  "database/sql"
-  "fmt"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
 
-  _ "github.com/lib/pq"
+	// "github.com/beego/beego/v2/server/web"
+
+	"github.com/beego/beego/v2/client/httplib"
 )
 
-const (
-  host     = "localhost"
-  port     = 5432
-  user     = "postgres"
-  password = "root"
-  dbname   = "Scrapydata1"
-)
+type Data []struct {
+
+UserID string `json:"userId"`
+ID string `json:"id"`
+Title string `json:"title"`
+Body string `json:"body"`
+
+}
+
+type Data1 []struct {
+ID string `json:"id"`
+Url string `json:"url"`
+Width string `json:"width"`
+Height string `json:"height"`
+Mime_type string `json:"mime_type"`
+Entities string `json:"entities"`
+Breeds string `json:"breeds"`
+Animals string `json:"animals"`
+Categories string `json:"categories"`
+}
 
 func main() {
-  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-    "password=%s dbname=%s sslmode=disable",
-    host, port, user, password, dbname)
-  db, err := sql.Open("postgres", psqlInfo)
-  if err != nil {
-    panic(err)
-  }
-  defer db.Close()
 
-//   sqlStatement := `
-// INSERT INTO users (age, email, first_name, last_name)
-// VALUES ($1, $2, $3, $4)
-// RETURNING id`
-//   id := 0
-//   err = db.QueryRow(sqlStatement, 30, "jon@calhoun.io", "Jonathan", "Calhoun").Scan(&id)
-
-//   fmt.Println(id)
-//   if err != nil {
-//     panic(err)
-//   }
-
-rows, err := db.Query("SELECT id, title, amenities,location,price FROM newdata")
-  if err != nil {
-    panic(err)
-  }
-  defer rows.Close()
-  for rows.Next() {
-    var id int
-    var title string
-    var amenities string
-    var location string
-    var price string
-    err = rows.Scan(&id, &title, &amenities,&location,&price)
-    if err != nil {
-      // handle this error
-      panic(err)
-    }
-    // fmt.Println(id, title,amenities,location,price)
-    fmt.Println(id, title,amenities,location,price)
-  }
-  // get any error encountered during iteration
-  err = rows.Err()
-  if err != nil {
-    panic(err)
-  }
-  
+req := httplib.Get("https://api.thecatapi.com/v1/images/search?limit=6&page=3&breed=Abyssinian")
+req.Header("x-api-key", `31cf5af0-bb4d-4275-971c-0e161cbdfa0b`)
+// req.Param("username", "astaxie")
+// req.Param("password", "123456")
+resp, err := req.Response()
+if err != nil {
+log.Fatal(err)
 }
+
+body, _ := ioutil.ReadAll(resp.Body)
+
+var f Data1
+
+err = json.Unmarshal(body, &f)
+
+//fmt.Printf("%+v", f[0].Body)
+
+for i := 0; i < 6; i++ {
+
+fmt.Println(f[i].Url)
+}
+// fmt.Printf("Body : %s", body)
+    // web.Run()
+}
+
